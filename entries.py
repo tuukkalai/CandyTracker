edd212fd8cc8916429bfc4ee7ffe7ed531a4c401
Tuukka Laitinen <tuukka.mj.laitinen@gmail.com>
2020-10-05 00:08:31 +0300

    Added missing additional stats value

--- entries.py
+++ entries.py
@@ -23,7 +23,7 @@ def get_sum_of_days():
 
 def get_additional_user_data():
     user = users.user_id()
-    sql = "SELECT COUNT(e.id) AS count, ROUND(CAST(SUM(c.size) AS NUMERIC)/1000, 2) AS sum, ROUND(CAST(SUM(c.sugar) AS NUMERIC)/1000, 2) AS sugar FROM entries e INNER JOIN candies c ON e.candy_id=c.id WHERE e.user_id=:user"
+    sql = "SELECT COUNT(e.id) AS count, ROUND(CAST(SUM(c.size) AS NUMERIC)/1000, 2) AS sum, ROUND(CAST(SUM(c.sugar) AS NUMERIC)/1000, 2) AS sugar, (SELECT SUM(c.size) FROM candies c INNER JOIN entries e ON e.candy_id=c.id WHERE e.user_id=:user AND e.entry_time > CURRENT_DATE-7)-(SELECT SUM(c.size) FROM candies c INNER JOIN entries e ON e.candy_id=c.id WHERE e.user_id=:user AND e.entry_time <= CURRENT_DATE-7 AND e.entry_time > CURRENT_DATE-14) AS diff FROM entries e INNER JOIN candies c ON e.candy_id=c.id WHERE e.user_id=:user"
     result = db.session.execute(sql,{"user":user})
     data = result.fetchone()
     return data
