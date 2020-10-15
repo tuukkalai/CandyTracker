792f58956c166c414bbde4e725a3e732181c8061
Tuukka Laitinen <tuukka.mj.laitinen@gmail.com>
2020-10-15 09:08:16 +0300

    "Remove members from groups" logic added to groups.py and users.py

--- users.py
+++ users.py
@@ -18,6 +18,14 @@ def is_admin():
         return True
     return False
 
+def is_group_admin(group_id):
+		sql = "SELECT members[0] FROM groups WHERE id=:group_id"
+		result = db.session.execute(sql, {"group_id":group_id})
+		admin = result.fetchone() # or fetchone()[0] ??
+		if user_id() == admin:
+				return True
+		return False
+
 def user_id():
     return session.get("user_id",0)
 
